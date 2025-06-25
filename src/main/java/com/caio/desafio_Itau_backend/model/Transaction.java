20814c4e27271787d6d65550df90ee9a18b74c23
Caio Prado <cpssouza7@gmail.com>
2025-06-25 15:25:51 -0300

    Feat: Criado Transaction.java

---
+++ src/main/java/com/caio/desafio_Itau_backend/model/Transaction.java
@@ -0,0 +1,23 @@
+package com.caio.desafio_Itau_backend.model;
+
+import java.time.OffsetDateTime;
+
+public class Transaction {
+    
+    private double valor;
+    private OffsetDateTime dataHora;
+
+    public Transaction(final double valor, final OffsetDateTime dataHora){
+        this.valor=valor;
+        this.dataHora=dataHora;
+    }
+
+    public double getValor(){
+        return valor;
+    }
+
+    public OffsetDateTime getDataHora(){
+        return dataHora;
+    }
+    
+}
