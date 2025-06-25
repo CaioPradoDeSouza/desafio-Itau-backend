540e240e8305fc1c46536fbeccd807dbcfb2180b
Caio Prado <cpssouza7@gmail.com>
2025-06-25 15:18:55 -0300

    first commit

---
+++ src/test/java/com/caio/desafio_Itau_backend/DesafioItauBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio.desafio_Itau_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
